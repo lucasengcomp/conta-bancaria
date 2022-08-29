e335003c7649a88dbc883c716a0f9c2c9d47a3cf
Lucas Galvao <lucas.engcomp@outlook.com>
2022-08-29 13:33:17 -0300

    Incluido teste deposita descontando 2% do valor depositado. Refatorado pacotes.

---
+++ backend/src/com/br/testes/entities/AccountTest.java
@@ -0,0 +1,20 @@
+package com.br.testes.entities;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import com.br.entities.Account;
+
+public class AccountTest {
+
+    @Test
+    public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+    	double amount = 200.0;
+    	double expectedValue = 196.0;
+    	var acc = new Account(1L, 0.0);
+    	
+    	acc.deposit(amount);
+    	
+    	Assertions.assertEquals(expectedValue, acc.getBalance());
+    }
+}
